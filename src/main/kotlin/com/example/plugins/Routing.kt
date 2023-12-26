68ce188f445664bb685a254d1f073042477167b6
redacted <vmlinuz@duck.com>
2023-12-26 17:31:27 -0600

    Making bud!

---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,11 @@
+package com.example.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        staticResources("/", "main")
+    }
+}
